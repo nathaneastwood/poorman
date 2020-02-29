2ff3fee20ebd47292d9b946533173f022dcfe2a2
nathaneastwood <nathan.eastwood@icloud.com>
2020-02-29 15:19:01 +0100

    feat: Allow integer pulls
    Add markdown to roxygen

--- R/select.R
+++ R/select.R
@@ -19,17 +19,27 @@ select <- function(.data, ...) {
   extract(.data, , cols, drop = FALSE)
 }
 
-#' Pull
+#' Pull out a single variable
 #'
 #' @param .data A `data.frame`.
-#' @param ... The name of the column to extract
+#' @param var A variable specified as:
+#' * a literal variable name
+#' * a positive integer, giving the position counting from the left
+#' * a negative integer, giving the position counting from the right
+#'
+#' The default returns the last column (on the assumption that's the column you've created most recently).
 #'
 #' @examples
-#' pull(mtcars, mpg)
-#' mtcars %>% pull(disp)
+#' mtcars %>% pull(-1)
+#' mtcars %>% pull(1)
+#' mtcars %>% pull(cyl)
 #'
 #' @export
-pull <- function(.data, var) {
+pull <- function(.data, var = -1) {
   var <- deparse(substitute(var))
+  if (!(var %in% colnames(.data)) & grepl("^[[:digit:]]+L|[[:digit:]]", var)) {
+    var <- as.integer(gsub("L", "", var))
+    var <- ifelse(var < 1L, extract(rev(colnames(.data)), abs(var)), extract(colnames(.data), var))
+  }
   extract2(.data, var)
 }
--- inst/tinytest/test_pull.R
+++ inst/tinytest/test_pull.R
@@ -8,3 +8,27 @@ expect_equal(
   mtcars[, "mpg"],
   info = "Test pulling a single column"
 )
+
+expect_equal(
+  mtcars %>% pull(1L),
+  mtcars[, "mpg"],
+  info = "Test pulling a single column with an integer"
+)
+
+expect_equal(
+  mtcars %>% pull(1),
+  mtcars[, "mpg"],
+  info = "Test pulling a single column with a numeric"
+)
+
+expect_equal(
+  mtcars %>% pull(-1L),
+  mtcars[, "carb"],
+  info = "Test pulling a single column with a negative integer"
+)
+
+expect_equal(
+  mtcars %>% pull(-1),
+  mtcars[, "carb"],
+  info = "Test pulling a single column with a negative numeric"
+)
