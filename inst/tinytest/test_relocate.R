f7c173dddc861d76cb3f568a3f0ace2e7b4ad1d4
nathaneastwood <nathan.eastwood@icloud.com>
2020-04-04 21:13:07 +0100

    test: Add tests for relocate

---
+++ inst/tinytest/test_relocate.R
@@ -0,0 +1,27 @@
+expect_equal(
+  mtcars %>% relocate(drat),
+  mtcars[, c("drat", colnames(mtcars)[!colnames(mtcars) %in% "drat"])],
+  info = "Test default relocate() moves column to the start of the data.frame"
+)
+
+expect_equal(
+  mtcars %>% relocate(drat, .before = cyl),
+  mtcars[, c("mpg", "drat", "cyl", "disp", "hp", "wt", "qsec", "vs", "am", "gear", "carb")],
+  info = "Test .before functionality for single variable"
+)
+
+expect_equal(
+  mtcars %>% relocate(drat, .after = cyl),
+  mtcars[, c("mpg", "cyl", "drat", "disp", "hp", "wt", "qsec", "vs", "am", "gear", "carb")],
+  info = "Test .after functionality for single variable"
+)
+
+expect_equal(
+  mtcars %>% relocate(drat, gear, .after = cyl),
+  mtcars[, c("mpg", "cyl", "drat", "gear", "disp", "hp", "wt", "qsec", "vs", "am", "carb")],
+  info = "Test .after functionality for multiple variables"
+)
+
+expect_error(
+  mtcars %>% relocate(gear, .after = mpg, .before = cyl)
+)
