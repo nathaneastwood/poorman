44579f65fb7e15f14b34982db217cf57abcc2185
nathaneastwood <nathan.eastwood@icloud.com>
2020-06-07 14:52:37 +0100

    feat: Add near()
    Closes #36

---
+++ R/near.R
@@ -0,0 +1,16 @@
+#' Compare two numeric vectors
+#'
+#' This is a safe way of comparing if two vectors of floating point numbers are (pairwise) equal.  This is safer than
+#' using `==`, because it has a built in tolerance.
+#'
+#' @param x,y Numeric vectors to compare
+#' @param tol Tolerance of comparison.
+#'
+#' @examples
+#' sqrt(2) ^ 2 == 2
+#' near(sqrt(2) ^ 2, 2)
+#'
+#' @export
+near <- function(x, y, tol = .Machine$double.eps^0.5) {
+  abs(x - y) < tol
+}
---
+++ inst/tinytest/test_near.R
@@ -0,0 +1,4 @@
+expect_true(
+  near(sqrt(2)^2, 2),
+  info = "near() accepts nearby fp values"
+)
