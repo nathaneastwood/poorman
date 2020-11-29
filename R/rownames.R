69d7f9195d98b81e5444eccb9139e83667d4452f
nathaneastwood <nathan.eastwood@icloud.com>
2020-11-29 14:06:14 +0100

    feat: Add additional tools for working with rownames
    Closes #66

--- R/rownames.R
+++ R/rownames.R
@@ -1,20 +1,77 @@
 #' Tools for working with row names
 #'
+#' In some quarters, it is considered best to avoid row names, because they are effectively a character column with
+#' different semantics than every other column.
+#' These functions allow to you detect if a `data.frame` has row names (`has_rownames()`), remove them
+#' (`remove_rownames()`), or convert them back-and-forth between an explicit column (`rownames_to_column()` and
+#' `column_to_rownames()`). Also included is `rowid_to_column()`, which adds a column at the start of the dataframe of
+#' ascending sequential row ids starting at 1. Note that this will remove any existing row names.
+#'
 #' @param .data A `data.frame`.
 #' @param var `character(1)`. The name of the column to use for row names.
 #'
-#' @returns A `data.frame`
+#' @returns
+#' * `column_to_rownames()` always returns a `data.frame`.
+#' * `has_rownames()` returns a `logical(1)`.
+#' * All other functions return an object of the same class as the input.
 #'
 #' @examples
-#' mtcars %>% rownames_to_column()
+#' # Detect row names
+#' has_rownames(mtcars)
+#' has_rownames(iris)
+#'
+#' # Remove row names
+#' remove_rownames(mtcars) %>% has_rownames()
+#'
+#' # Convert between row names and column
+#' mtcars <- rownames_to_column(mtcars, var = "car")
+#' column_to_rownames(mtcars, var = "car") %>% head()
+#'
+#' # Adding rowid as a column
+#' rowid_to_column(iris) %>% head()
 #'
 #' @name rownames
 #' @export
 rownames_to_column <- function(.data, var = "rowname") {
   check_is_dataframe(.data)
   col_names <- colnames(.data)
-  if (var %in% col_names) stop("Column `", var, "` already exists in `.data`")
+  if (var %in% col_names) stop("Column `", var, "` already exists in `.data`.")
   .data[, var] <- rownames(.data)
   rownames(.data) <- NULL
   .data[, c(var, setdiff(col_names, var))]
+}
+
+#' @rdname rownames
+#' @export
+rowid_to_column <- function(.data, var = "rowid") {
+  check_is_dataframe(.data)
+  if (var %in% colnames(.data)) stop("Column `", var, "` already exists in `.data`.")
+  args <- list(.data = .data, quote(row_number()), .before = 1L)
+  names(args)[2L] <- var
+  remove_rownames(do.call(mutate, args))
+}
+
+#' @rdname rownames
+#' @export
+column_to_rownames <- function(.data, var = "rowname") {
+  check_is_dataframe(.data)
+  if (has_rownames(.data)) stop("`.data` already has rownames")
+  if (!(var %in% colnames(.data))) stop("Column `", var, "` could not be found in `.data`.")
+  rownames(.data) <- .data[[var]]
+  .data[[var]] <- NULL
+  .data
+}
+
+#' @rdname rownames
+#' @export
+remove_rownames <- function(.data) {
+  check_is_dataframe(.data)
+  rownames(.data) <- NULL
+  .data
+}
+
+#' @rdname rownames
+#' @export
+has_rownames <- function(.data) {
+  .row_names_info(.data) > 0L
 }
--- inst/tinytest/test_rownames.R
+++ inst/tinytest/test_rownames.R
@@ -1,15 +1,43 @@
-expect_equal(
-  mtcars %>% rownames_to_column(),
-  {
-    res <- mtcars
-    cols <- colnames(res)
-    res[, "rowname"] <- rownames(res)
-    rownames(res) <- NULL
-    res[, c("rowname", cols)]
-  },
-  info = "Test rownames to column"
+# has_rownames()
+expect_false(has_rownames(iris), info = "has_rownames() returns FALSE correctly")
+expect_true(has_rownames(mtcars), info = "has_rownames() returns TRUE correctly")
+expect_false(has_rownames(1:10), info = "has_rownames() works on other objects, returning FALSE")
+
+# remove_rownames()
+expect_false(has_rownames(remove_rownames(mtcars)), info = "remove_rownames() acts accordingly")
+expect_false(
+  has_rownames(remove_rownames(iris)),
+  info = "remove_rownames() acts accordingly for datasets without rownames"
 )
 
-expect_error(
-  mtcars %>% rownames_to_column("mpg")
+# rowid_to_column()
+res <- rowid_to_column(mtcars)
+expect_false(has_rownames(res), info = "rownames are correctly removed")
+expect_equal(
+  mtcars %>% group_by(am) %>% rowid_to_column() %>% class(),
+  c("grouped_data", "data.frame"),
+  info = "rowid_to_column() retains classes"
+)
+expect_equal(res$rowid, seq_len(nrow(mtcars)), info = "rowid is correctly set")
+
+# rownames_to_column()
+res <- rownames_to_column(mtcars)
+expect_false(has_rownames(res), info = "rownames have indeed been removed")
+expect_equal(res$rowname, rownames(mtcars), info = "rownames have been correctly converted to a column")
+expect_equal(
+  mtcars %>% group_by(am) %>% rownames_to_column() %>% class(),
+  c("grouped_data", "data.frame"),
+  info = "rownames_to_column() retains class"
+)
+
+# column_to_rownames()
+res <- column_to_rownames(res, "rowname")
+expect_true(has_rownames(res), info = "column_to_rownames() does set rownames")
+expect_equal(rownames(res), rownames(mtcars), info = "rownames are correctly set")
+expect_equal(res, mtcars)
+expect_false("rowname" %in% colnames(res), info = "The old column is removed")
+expect_equal(
+  mtcars %>% group_by(am) %>% rownames_to_column() %>% column_to_rownames("rowname") %>% class(),
+  c("grouped_data", "data.frame"),
+  info = "column_to_rownames() retains class"
 )
